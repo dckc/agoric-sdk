3ac95d7ddfdb07693282fc89fd7cf563075cb8a9
Dan Connolly <dckc@madmode.com>
2019-09-02 22:46:23 -0500

    xs: refine test reporting

--- test-xs-main.js
+++ test-xs-main.js
@@ -1,18 +1,26 @@
 /* global trace */
 
 import { test } from 'tape-promise/tape';
+import { setTimeout } from 'xs-platform/moddableTimer';
 
 import testMarshal from './test/test-marshal';
 
 export default function main() {
-  trace('hi from main\n');
+  trace('# hi from main\n');
+  const htest = test.createHarness();
 
-  test('hello world tape', t => {
-    t.equal(1 + 1, 2);
-    t.end();
-  });
+  testMarshal();  // ISSUE: htest is ambient
 
-  testMarshal();
+  setTimeout(() => {
+    report(htest.summary(), (txt) => { trace(txt + '\n'); });
+  }, 1);
+  trace('# bye from main\n');
+}
 
-  trace('bye from main\n');
+// ISSUE: belongs in tape
+function report({ total, pass, fail }, writeln) {
+  writeln(`1..${total}`);
+  writeln(`# tests ${total}`);
+  writeln(`# pass  ${pass}`);
+  writeln(`# fail  ${fail}`);
 }
