f73066121575eb346c651523effcec83d14ee4de
Dan Connolly <dckc@madmode.com>
2019-09-08 17:52:28 -0500

    path stubs: isAbsolute, resolve

--- xs-platform/path.js
+++ xs-platform/path.js
@@ -1,1 +1,16 @@
-export default {};
+export function isAbsolute(path) {
+  return path.startsWith('/');
+}
+
+export function resolve(base, path) {
+  if (path.startsWith('/')) {
+    return path;
+  }
+  if (path.startsWith('.')) {
+    throw new Error(`not implemented: resolve(_, ${path})`);
+  }
+  const sep = base.endsWith('/') ? '' : '/';
+  return base + sep + path;
+}
+
+export default { isAbsolute, resolve };
