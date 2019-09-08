1a3addaba7ac0f95db85b5f86f63712d0e7ebcde
Dan Connolly <dckc@madmode.com>
2019-09-08 17:52:56 -0500

    fs shim: readdirSync, statSync

--- xs-platform/fs.js
+++ xs-platform/fs.js
@@ -1,1 +1,27 @@
-export default {};
+// ref https://github.com/Moddable-OpenSource/moddable/blob/public/examples/files/files/main.js
+
+// eslint-disable-next-line import/no-unresolved
+import { File, Iterator } from 'file';
+
+export function readdirSync(path) {
+  const out = [];
+  const iterator = new Iterator(path);
+  let item;
+  // eslint-disable-next-line no-cond-assign
+  while ((item = iterator.next())) {
+    const isFile = typeof item.length !== 'undefined';
+    const dirEnt = {
+      name: item.name,
+      isFile: () => isFile,
+    };
+    out.push(dirEnt);
+  }
+  return out;
+}
+
+export function statSync(path) {
+  const access = new File(path, false);
+  return { length: access.length };
+}
+
+export default { readdirSync, statSync };
