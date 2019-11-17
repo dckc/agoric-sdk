94493c85543ea99ec840ec05b96077b4822e9a50
Dan Connolly <dckc@madmode.com>
2019-11-16 22:05:01 -0600

    test-devices: factor out ambient access to source code
    ... using nodeSourceAccess() etc.

--- test/test-devices.js
+++ test/test-devices.js
@@ -1,6 +1,6 @@
 import { test } from 'tape-promise/tape';
 import harden from '@agoric/harden';
-import { buildVatController } from '../src/index';
+import { buildVatControllerRd, nodeSourceAccess } from '../src/index';
 import { buildStorageInMemory } from '../src/hostStorage';
 import { buildMailboxStateMap, buildMailbox } from '../src/devices/mailbox';
 import buildCommand from '../src/devices/command';
@@ -13,13 +13,20 @@ function capargs(args, slots = []) {
   return capdata(JSON.stringify(args), slots);
 }
 
+export default function runTests({
+  requireAbsPath,
+  resolveModule,
+  resolveTestDir,
+}) {
+
 async function test0(t, withSES) {
-  const config = {
+  const configRd = {
     vats: new Map(),
-    devices: [['d0', require.resolve('./files-devices/device-0'), {}]],
-    bootstrapIndexJS: require.resolve('./files-devices/bootstrap-0'),
+    devices: [['d0', resolveModule('./files-devices/device-0'), {}]],
+    bootstrapIndexJSRd: resolveModule('./files-devices/bootstrap-0'),
+    requireAbsPath,
   };
-  const c = await buildVatController(config, withSES);
+  const c = await buildVatControllerRd(configRd, withSES);
   await c.step();
   // console.log(util.inspect(c.dump(), { depth: null }));
   t.deepEqual(JSON.parse(c.dump().log[0]), [
@@ -46,20 +53,21 @@ test('d0 without SES', async t => {
 
 async function test1(t, withSES) {
   const sharedArray = [];
-  const config = {
+  const configRd = {
     vats: new Map(),
     devices: [
       [
         'd1',
-        require.resolve('./files-devices/device-1'),
+        resolveModule('./files-devices/device-1'),
         {
           shared: sharedArray,
         },
       ],
     ],
-    bootstrapIndexJS: require.resolve('./files-devices/bootstrap-1'),
+    bootstrapIndexJSRd: resolveModule('./files-devices/bootstrap-1'),
+    requireAbsPath,
   };
-  const c = await buildVatController(config, withSES);
+  const c = await buildVatControllerRd(configRd, withSES);
   await c.step();
   c.queueToVatExport('_bootstrap', 'o+0', 'step1', capargs([]));
   await c.step();
@@ -82,15 +90,16 @@ test('d1 without SES', async t => {
 });
 
 async function test2(t, mode, withSES) {
-  const config = {
+  const configRd = {
     vats: new Map(),
-    devices: [['d2', require.resolve('./files-devices/device-2'), {}]],
-    bootstrapIndexJS: require.resolve('./files-devices/bootstrap-2'),
+    devices: [['d2', resolveModule('./files-devices/device-2'), {}]],
+    bootstrapIndexJSRd: resolveModule('./files-devices/bootstrap-2'),
+    requireAbsPath,
   };
-  config.vats.set('left', {
-    sourcepath: require.resolve('./files-devices/vat-left.js'),
+  configRd.vats.set('left', {
+    sourceRd: resolveModule('./files-devices/vat-left.js'),
   });
-  const c = await buildVatController(config, withSES, [mode]);
+  const c = await buildVatControllerRd(configRd, withSES, [mode]);
   await c.step();
   if (mode === '1') {
     t.deepEqual(c.dump().log, ['calling d2.method1', 'method1 hello', 'done']);
@@ -183,16 +192,17 @@ test('d2.5 without SES', async t => {
 
 async function testState(t, withSES) {
   const { storage, getState } = buildStorageInMemory();
-  const config = {
+  const configRd = {
     vats: new Map(),
-    devices: [['d3', require.resolve('./files-devices/device-3'), {}]],
-    bootstrapIndexJS: require.resolve('./files-devices/bootstrap-3'),
+    devices: [['d3', resolveModule('./files-devices/device-3'), {}]],
+    bootstrapIndexJSRd: resolveModule('./files-devices/bootstrap-3'),
     hostStorage: storage,
+    requireAbsPath,
   };
 
   // The initial state should be missing (null). Then we set it with the call
   // from bootstrap, and read it back.
-  const c1 = await buildVatController(config, withSES, ['write+read']);
+  const c1 = await buildVatControllerRd(configRd, withSES, ['write+read']);
   const d3 = c1.deviceNameToID('d3');
   await c1.run();
   t.deepEqual(c1.dump().log, ['undefined', 'w+r', 'called', 'got {"s":"new"}']);
@@ -214,13 +224,14 @@ test('device state without SES', async t => {
 async function testMailboxOutbound(t, withSES) {
   const s = buildMailboxStateMap();
   const mb = buildMailbox(s);
-  const config = {
+  const configRd = {
     vats: new Map(),
-    devices: [['mailbox', mb.srcPath, mb.endowments]],
-    bootstrapIndexJS: require.resolve('./files-devices/bootstrap-2'),
+    devices: [['mailbox', resolveModule(mb.srcPath), mb.endowments]],
+    bootstrapIndexJSRd: resolveModule('./files-devices/bootstrap-2'),
+    requireAbsPath,
   };
 
-  const c = await buildVatController(config, withSES, ['mailbox1']);
+  const c = await buildVatControllerRd(configRd, withSES, ['mailbox1']);
   await c.run();
   t.deepEqual(s.exportToData(), {
     peer1: {
@@ -255,15 +266,16 @@ test('mailbox outbound with SES', async t => {
 async function testMailboxInbound(t, withSES) {
   const s = buildMailboxStateMap();
   const mb = buildMailbox(s);
-  const config = {
+  const configRd = {
     vats: new Map(),
-    devices: [['mailbox', mb.srcPath, mb.endowments]],
-    bootstrapIndexJS: require.resolve('./files-devices/bootstrap-2'),
+    devices: [['mailbox', resolveModule(mb.srcPath), mb.endowments]],
+    bootstrapIndexJSRd: resolveModule('./files-devices/bootstrap-2'),
+    requireAbsPath,
   };
 
   let rc;
 
-  const c = await buildVatController(config, withSES, ['mailbox2']);
+  const c = await buildVatControllerRd(configRd, withSES, ['mailbox2']);
   await c.run();
   rc = mb.deliverInbound('peer1', [[1, 'msg1'], [2, 'msg2']], 0);
   t.ok(rc);
@@ -339,13 +351,14 @@ test('mailbox inbound with SES', async t => {
 async function testCommandBroadcast(t, withSES) {
   const broadcasts = [];
   const cm = buildCommand(body => broadcasts.push(body));
-  const config = {
+  const configRd = {
     vats: new Map(),
-    devices: [['command', cm.srcPath, cm.endowments]],
-    bootstrapIndexJS: require.resolve('./files-devices/bootstrap-2'),
+    devices: [['command', resolveModule(cm.srcPath), cm.endowments]],
+    bootstrapIndexJSRd: resolveModule('./files-devices/bootstrap-2'),
+    requireAbsPath,
   };
 
-  const c = await buildVatController(config, withSES, ['command1']);
+  const c = await buildVatControllerRd(configRd, withSES, ['command1']);
   await c.run();
   t.deepEqual(broadcasts, [{ hello: 'everybody' }]);
 
@@ -362,13 +375,14 @@ test('command broadcast with SES', async t => {
 
 async function testCommandDeliver(t, withSES) {
   const cm = buildCommand(() => {});
-  const config = {
+  const configRd = {
     vats: new Map(),
-    devices: [['command', cm.srcPath, cm.endowments]],
-    bootstrapIndexJS: require.resolve('./files-devices/bootstrap-2'),
+    devices: [['command', resolveModule(cm.srcPath), cm.endowments]],
+    bootstrapIndexJSRd: resolveModule('./files-devices/bootstrap-2'),
+    requireAbsPath,
   };
 
-  const c = await buildVatController(config, withSES, ['command2']);
+  const c = await buildVatControllerRd(configRd, withSES, ['command2']);
   await c.run();
 
   t.deepEqual(c.dump().log.length, 0);
@@ -398,3 +412,42 @@ test('command deliver without SES', async t => {
 test('command deliver with SES', async t => {
   await testCommandDeliver(t, true);
 });
+}
+
+
+function testAccess( // ISSUE: duplicated from test-controller.js
+  { requireAbsPath, makeRdModule, makeRdDir },
+  { path, dirname, requireResolve },
+) {
+  return harden({
+    requireAbsPath,
+    resolveModule(specifier) {
+      return makeRdModule(requireResolve(specifier));
+    },
+    resolveTestDir(dir) {
+      return makeRdDir(path.resolve(dirname, dir));
+    },
+  });
+}
+
+/** Access ambient authority only if invoked as script. */
+/* global require, module, __dirname */
+if (typeof require !== 'undefined' && typeof module !== 'undefined') {
+  /* eslint-disable global-require */
+  runTests(
+    testAccess(
+      nodeSourceAccess({
+        requireModule: require,
+        fs: require('fs'),
+        path: require('path'),
+        rollup: require('rollup').rollup,
+        resolvePlugin: require('rollup-plugin-node-resolve'),
+      }),
+      {
+        dirname: __dirname,
+        path: require('path'),
+        requireResolve: require.resolve,
+      },
+    ),
+  );
+}
