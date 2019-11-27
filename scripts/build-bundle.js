6db19c41301d80a750d5e83f931b84eb64d8175a
Dan Connolly <dckc@madmode.com>
2019-11-26 19:34:30 -0600

    build-bundle.js: default to bundling genesis devices

--- scripts/build-bundle.js
+++ scripts/build-bundle.js
@@ -1,27 +1,35 @@
 import bundleSource from '@agoric/bundle-source';
 
-async function main(argv, { open, rollup, resolvePlugin, pathResolve }) {
+async function main(argv, pkg, { open, rollup, resolvePlugin, pathResolve }) {
   if (argv.length < 4) {
-    throw new Error('usage: bundle startFilename dest');
+    for (const dev of ['mailbox', 'command', 'timer']) {
+      const [startFilename, dest] = [`${pkg}/src/devices/${dev}-src.js`, `${pkg}/src/bundles/${dev}-src.js`];
+      await bundle1(startFilename, dest);
+    }
+  } else {
+    const [startFilename, dest] = argv.slice(2, 4);
+    await bundle1(startFilename, dest);
   }
-  const [startFilename, dest] = argv.slice(2, 4);
 
-  const { source, sourceMap } = await bundleSource(
-    startFilename,
-    'getExport',
-    { rollup, resolvePlugin, pathResolve },
-  );
-  const actualSource = `export default ${source}\n${sourceMap}`;
-  const f = await open(dest, 'w', 0o644);
-  await f.write(actualSource);
-  await f.close();
+  async function bundle1(startFilename, dest) {
+    console.log(`bundle: ${startFilename} -> ${dest}`);
+    const { source, sourceMap } = await bundleSource(
+      startFilename,
+      'getExport',
+      { rollup, resolvePlugin, pathResolve },
+    );
+    const actualSource = `export default ${source}\n${sourceMap}`;
+    const f = await open(dest, 'w', 0o644);
+    await f.write(actualSource);
+    await f.close();
+  }
+
 }
-
 
 
 /** Access ambient authority only if invoked as script. */
 if (typeof require !== 'undefined' && typeof module !== 'undefined') {
-  main(process.argv, {
+  main(process.argv, `${__dirname}/..`, {
     open: require('fs').promises.open,
     rollup: require('rollup').rollup,
     pathResolve: require('path').resolve,
