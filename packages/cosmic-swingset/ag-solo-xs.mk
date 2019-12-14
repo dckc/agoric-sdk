d310b97945dd502501acc7cb41033ac85f4daed5
Dan Connolly <dckc@madmode.com>
2019-12-13 18:37:10 -0600

    ag-solo-xs: adapt manifest to monorepo
    try an xs_modules symlink

--- packages/cosmic-swingset/ag-solo-xs.mk
+++ packages/cosmic-swingset/ag-solo-xs.mk
@@ -4,14 +4,19 @@ export PATH := $(MODDABLE)/build/bin/lin/release:$(PATH)
 PKG=cosmic-swingset
 
 
+# NOTE: yarn link n-readlines@@
+
 BASEDIR=t3
-release-build:
+release-build: xs_modules
 	mcconfig -p x-cli-lin ag-solo-xs-manifest.json
 	cd $(MODDABLE)/build/tmp/lin/release/$(PKG) && $(MAKE)
 	$(MODDABLE)/build/bin/lin/release/$(PKG) $(BASEDIR)
 
-debug-build:
+debug-build: xs_modules
 	mcconfig -d -p lin -m ag-solo-xs-manifest.json
+
+xs_modules: ../../node_modules
+	ln -s ../../node_modules $@
 
 clean:
 	-rm -rf $(MODDABLE)/build/tmp/lin/release/$(PKG)
