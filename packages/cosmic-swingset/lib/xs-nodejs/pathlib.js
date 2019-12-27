dd096cb0abc5d7fdeefe574bfee4b22f02fce030
Dan Connolly <dckc@madmode.com>
2019-12-27 15:04:39 -0600

    xs pathlib: reify file access as objects (copy)
    https://raw.githubusercontent.com/Agoric/agoric-sdk/757c3378481829fd085a80413cbfc00c91e8aeb1/packages/cosmic-swingset/xs-platform/pathlib.js
    
    packages/cosmic-swingset$ git log --follow --pretty='format:%C(auto)%h (%s, %ad)' xs-platform/pathlib.js
    1a27364 (clean up debug logging, Sat Dec 21 00:56:02 2019 -0600)
    83f3f39 (handle empty file in readlinesSync(), Fri Dec 20 23:42:16 2019 -0600)
    bf33a2d (xs pathlib: include filename in readFileSync() diagnostics, Fri Dec 13 20:18:22 2019 -0600)
    4f7dc64 (move xs manifest etc. into packages/cosmic-swingset, Fri Dec 13 17:41:34 2019 -0600)
    1ea6a96 (xs platform: flesh out pathlib, Wed Nov 27 02:16:29 2019 -0600)
    d45c934 (xs pathlib: bootstrap.js convention, Wed Nov 27 01:33:32 2019 -0600)
    17f409f (xs pathlib: bundleSource, Tue Nov 26 21:14:07 2019 -0600)
    7fa0a8d (xs pathlib: statSync, readlinesSync, Sun Nov 24 22:59:50 2019 -0600)
    258b178 (ag-solo on xs: hack-n-slash to parts of start(), Sun Nov 24 18:50:32 2019 -0600)

---
+++ packages/cosmic-swingset/lib/xs-nodejs/pathlib.js
@@ -0,0 +1,129 @@
+const harden = x => Object.freeze(x, true);
+
+export function makePath(filename, { File, Iterator }) {
+  const mk = there => makePath(there, { File, Iterator });
+
+  function readFileSync() {
+    let file;
+    let contents;
+    try {
+      file = new File(filename);
+      contents = file.read(String);
+    } catch(oops) {
+      // not sure why xs loses error messages, but without logging, we just get:
+      // xs-platform/pathlib.js:21: exception: throw!
+      console.log({ filename, message: oops.message });
+      throw(new Error(`${filename}: ${oops.message}`));
+    }
+    file.close();
+    return contents;
+  }
+
+  function withWriting(suffix, thunk) {
+    const tmpName = filename + suffix;
+    const file = new File(tmpName, true);
+    const fp = harden({
+      writeSync(value) {
+	file.write(value);
+      }
+    });
+    try {
+      thunk(fp);
+    } finally {
+      file.close();
+    }
+    File.rename(tmpName, filename);
+  }
+
+  function atomicReplace(contents) {
+    return new Promise(
+      (resolve, reject) => {
+	try {
+	  const tmp = mk(filename + '.tmp');
+	  const tmpF = new File(tmp, true);
+	  tmpF.write(contents);
+	  tmpF.close();
+	  File.rename(tmp, filename);
+	} catch(oops) {
+	  reject(oops);
+	  return;
+	}
+	resolve();
+      });
+  }
+
+  function readdirSync(options) {
+    const dirIter = new Iterator(filename);
+    let item;
+    const items = [];
+    while ((item = dirIter.next())) {
+      const f = typeof item.length === 'number';
+      items.push(harden({
+	name: item.name,
+	isFile: () => f,
+      }));
+    }
+    return items;
+  }
+
+  function readdir() {
+    return new Promise((resolve, reject) => {
+      try {
+	const names = readdirSync({}).map(item => item.name);
+	resolve(names);
+      } catch (oops) {
+	reject(oops);
+      }
+    });
+  }
+
+  function butLast(p) {
+    const pos = p.lastIndexOf('/');
+    return pos >= 0 ? p.slice(0, pos + 1) : p;
+  }
+
+  function bundleSource() {
+    let bundlePath;
+    const parts = filename.match(/vat(-)([^\.]+).js$/);
+    if (parts) {
+      bundlePath = `${butLast(filename)}vat_${parts[2]}-src.js`;
+    } else if (filename.match(/\/bootstrap.js$/)) {
+      bundlePath = `${butLast(filename)}bootstrap-src.js`;
+    } else {
+      throw new Error(`expected vat-NAME.js; got: ${filename}`);
+    }
+    console.log(`@@bundleSource ${filename} -> ${bundlePath}`);
+    const src = mk(bundlePath).readFileSync();
+    return {
+      source: src.replace(/^export default /, ''),
+      sourceMap: `//# sourceURL=${filename}\n`,
+    };
+  }
+
+  return harden({
+    toString() {
+      return filename;
+    },
+    resolve(...others) {
+      // ISSUE: support ../?
+      return mk([butLast(filename), ...others].join('/'));
+    },
+    join(...others) {
+      // ISSUE: support ../?
+      return mk([filename, ...others].join('/'));
+    },
+    statSync() {
+      new File(filename);
+      return harden({});
+    },
+    readFileSync,
+    readlinesSync() {
+      return readFileSync().replace(/\n$/, '').split('\n');
+    },
+    readdirSync,
+    readdir,
+    bundleSource,
+    atomicReplace,
+    withWriting,
+  });
+}
