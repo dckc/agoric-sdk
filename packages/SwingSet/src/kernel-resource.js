768ad018058fff962d99f6b62cd5a8628bec1e33
Dan Connolly <dckc@madmode.com>
2019-12-27 22:25:57 -0600

    emulate bundles/kernel using xs Resource API

---
+++ packages/SwingSet/src/kernel-resource.js
@@ -0,0 +1,21 @@
+/* global Resource */
+import harden from '@agoric/harden';
+
+import Resource from "Resource";
+
+/**
+ * In the original node.js approach, the bundles/kernel module exports
+ * a function and controller coerces it to string to get its
+ * source.
+ *
+ * Here we load the kernel source from "ROM" as a Resource and
+ * convert from ESM syntax a la getExport from bundle-source.
+ */
+export default harden({
+  toString() {
+    const kernelROM = new Resource("agoric.kernel.js");
+    const kernelModText = String.fromArrayBuffer(kernelROM.slice(0));
+    const kernelExpr = kernelModText.slice('export default '.length);
+    return kernelExpr;
+  },
+});
