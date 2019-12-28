5a5dde8b9c739424e30cc49e16a5517c4f1abb1a
Dan Connolly <dckc@madmode.com>
2019-12-27 22:21:26 -0600

    ag-solo-xs: tidy console stuff

--- packages/cosmic-swingset/bin/ag-solo-xs.js
+++ packages/cosmic-swingset/bin/ag-solo-xs.js
@@ -1,12 +1,7 @@
 /* global Compartment, trace */
-import { makeConsole } from 'xs-platform/console';
-
-const harden = x => Object.freeze(x, true);
-
 import solo from './lib/ag-solo/main';
 
 export default async function main() {
-  const console = makeConsole();
   console.log(`in bin/ag-solo-xs...`);
 
   try {
