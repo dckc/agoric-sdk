e412fbaff0eb4d7b44daf641f2c9ee7afbd3dd55
Dan Connolly <dckc@madmode.com>
2019-12-28 18:25:19 -0600

    bundle-source work-alike ok for scenario3
    includes a couple kludges
    
      - treat genesis vats as data Resources
      - bundle devices, scenario vats in makefile
      - path kludge

--- packages/cosmic-swingset/ag-solo-xs.mk
+++ packages/cosmic-swingset/ag-solo-xs.mk
@@ -5,7 +5,7 @@ PKG=cosmic-swingset
 
 BASEDIR=t3
 
-check-imports: $(MODDABLE)/build/bin/lin/release/cosmic-swingset
+check-start: $(MODDABLE)/build/bin/lin/release/cosmic-swingset ../SwingSet/src/bundles/mailbox-src.js t3/vats/bootstrap-src.js
 	cd $(BASEDIR) && $(MODDABLE)/build/bin/lin/release/cosmic-swingset start
 
 $(MODDABLE)/build/bin/lin/release/cosmic-swingset: ./bin/ag-solo-xs.js lib/ag-solo/*.js lib/xs-npm/*.js lib/xs-nodejs/*.js lib/ag-solo-todo/*.js
