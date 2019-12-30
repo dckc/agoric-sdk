4f54113e6b9051de89a69bb6a0d725df7d1541bf
Dan Connolly <dckc@madmode.com>
2019-12-30 13:10:02 -0600

    grumble... bundle-source got ignored

---
+++ packages/cosmic-swingset/lib/xs-npm/bundle-source.js
@@ -0,0 +1,47 @@
+import harden from '@agoric/harden';
+
+import Resource from 'Resource';
+
+import fs from 'fs';  // beware: powerful!
+
+const genesisVats = {
+  './vats/vat-tp': '@agoric.vattp.js',
+  './mailbox-src': '@agoric.mailbox-src.js',
+  './command-src': '@agoric.command-src.js',
+  './timer-src': '@agoric.timer-src.js',
+  './vats/comms': '@agoric.comms.js',
+  './vats/vat-timerWrapper': '@agoric.vat-timerWrapper.js',
+};
+
+function butLast(p) {
+  const pos = p.lastIndexOf('/');
+  return pos >= 0 ? p.slice(0, pos + 1) : p;
+}
+
+export default function bundleSource(path) {
+  console.log('@@bundle-source:', path);
+  let vatModText;
+  const resName = genesisVats[path];
+  if (resName) {
+    const vatROM = new Resource(resName);
+    vatModText = String.fromArrayBuffer(vatROM.slice(0));
+  } else {
+    let bundlePath;
+    const parts = path.match(/vat(-)([^\.]+).js$/);
+    if (parts) {
+      bundlePath = `${butLast(path)}vat_${parts[2]}-src.js`;
+    } else if (path.match(/\/bootstrap.js$/)) {
+      bundlePath = `${butLast(path)}bootstrap-src.js`;
+    } else {
+      throw new Error(`expected vat-NAME.js; got: ${path}`);
+    }
+    console.log(`@@bundleSource ${path} -> ${bundlePath}`);
+    vatModText = fs.readFileSync(bundlePath);
+  }
+
+  const vatExpr = vatModText.slice('export default '.length);
+  return harden({
+    source: vatExpr,
+    sourceMap: `//# sourceURL=${path}\n`,
+  });
+}
