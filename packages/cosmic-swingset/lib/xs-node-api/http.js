0b9028e73f27cc57c90c11263c280b57a1f96c55
Dan Connolly <dckc@madmode.com>
2019-12-31 22:12:15 -0600

    websocket shim between moddable sdk and npm ws (WIP)
     - manifest: add xs-net/websocket and dependencies (base64...)
     - event handling
     - TODO: actually handle upgrade (crypto)

--- packages/cosmic-swingset/lib/xs-node-api/http.js
+++ packages/cosmic-swingset/lib/xs-node-api/http.js
@@ -2,8 +2,33 @@ import {Server} from 'xs-net/http';  // beware: powerful!
 
 import harden from '@agoric/harden';
 
+
+function makeEmitter() {
+  const byName = {};
+
+  const later = thunk => Promise.resolve(null).then(thunk);
+
+  return harden({
+    on(name, handler) {
+      if (name in byName) {
+	byName[name].push(handler);
+      } else {
+	byName[name] = [handler];
+      }
+    },
+    emit(name, ...args) {
+      const handlers = byName[name];
+      if (!handlers) { return; }
+      for (const handler of handlers) {
+	later(() => handler(...args));
+      }
+    }
+  });
+}
+
 export function createServer(handler) {
   let server;
+  const events = makeEmitter();
 
   function listen(port, host, listening) {
     server = new Server({ host, port });
@@ -13,18 +38,43 @@ export function createServer(handler) {
 	this.request = {};
 	this.buf = { content: '', pos: 0 };
       }
-      const { request: req, buf } = this;
+      const { request: req, buf, socket } = this;
       switch (message) {
       case 2:
 	req.status = 200;
 	req.path = val1;
 	req.method = val2;
 	req.headers = {};
+	req.upgrade = false;
 	break;
       case 3:
 	req.headers[val1] = val2;
 	break;
+      case 4:
+	console.log('@@http shim got request head:', req);
+	if (req.headers['upgrade'] === 'websocket') {
+	  const peer = socket.get('REMOTE_IP').split(':');
+	  const [host, port] = peer;
+	  // socket.state has to stay mutable, so we can't harden nodeSocket
+	  // We don't rely on this structure after handling it
+	  // to the event handler, though.
+	  const nodeSocket = {
+	    remoteAddress: host,
+	    remotePort: port,
+	    _xs_socket: socket,
+	  };
+	  const nodeReq = {
+	    headers: req.headers,
+	    url: req.path,
+	    socket: nodeSocket,
+	  };
+	  const bodyHead = '';
+	  events.emit('upgrade', nodeReq, nodeSocket, bodyHead);
+	  req.upgrade = true;
+	}
+	break;
       case 8:
+	if (req.upgrade) { return undefined; }  // don't reply
 	harden(req);
 	let byName = {'content-type': 'text/plain'};
 	const resp = harden({
@@ -41,19 +91,24 @@ export function createServer(handler) {
 	    return resp;
 	  },
 	});
+	console.log('@@http shim calling handler with:', req);
 	handler(req, resp);
-	return harden({
+	const out = harden({
 	  status: req.status,
 	  headers: Object.entries(byName).flatMap(nv => nv),
 	  body: true,
 	});
+	console.log('@@http shim responding:', out);
+	return out;
       case 9:
 	if (buf.pos >= buf.content.length) {
+	  console.log('@@http shim: request done');
 	  return undefined;
 	}
 	const okToTx = val1;
 	const chunk = buf.content.slice(buf.pos, buf.pos + okToTx);
 	buf.pos += okToTx;
+	console.log('@@http shim chunk:', okToTx, chunk.slice(0, 16));
 	return chunk;
       }
       return undefined;
@@ -62,15 +117,7 @@ export function createServer(handler) {
   }
 
   return harden({
-    on(name, handler) {
-      switch (name) {
-      case 'upgrade':
-	console.error('http upgrade TODO');
-	break;
-      default:
-	throw new Error(`http.on(${name}) not implemented`);
-      }
-    },
+    on: events.on,
     listen,
   });
 }
--- packages/cosmic-swingset/lib/xs-npm/ws.js
+++ packages/cosmic-swingset/lib/xs-npm/ws.js
@@ -1,10 +1,96 @@
 import harden from '@agoric/harden';
 
-export function Server() {
+import xsws from "xs-net/websocket";
+
+
+const later = thunk => Promise.resolve(null).then(thunk);
+
+// ISSUE: factor out overlap with http.js?
+function makeEmitter() {
+  const byName = {};
+
   return harden({
     on(name, handler) {
-      console.error('ws.Server.on TODO');
+      if (name in byName) {
+	byName[name].push(handler);
+      } else {
+	byName[name] = [handler];
+      }
     },
+    emit(name, ...args) {
+      const handlers = byName[name];
+      if (!handlers) { return; }
+      for (const handler of handlers) {
+	later(() => handler(...args));
+      }
+    }
+  });
+}
+
+function WebSocket({ path, host, headers, protocol, socket }) {
+  const events = makeEmitter();
+  const xclient = new xsws.Client({ path, host, headers, protocol, socket });
+
+  function emit(name, ...args) {
+    events.emit(name, ...args);
+  }
+
+  xclient.callback = function(message, value) {
+    switch (message) {
+    case xsws.Client.connect:
+      emit('connection');
+      break;
+    case xsws.Client.receive:
+      emit('message', value);
+      break;
+    case xsws.Client.disconnect:
+      emit('close');
+    }
+  };
+
+  const OPEN = 1;
+  return harden({
+    connection: socket,
+    readyState: OPEN,
+    OPEN,
+    emit,
+    on(name, ...args) {
+      events.on(name, ...args);
+    },
+    send(message) {
+      xclient.write(message);
+    },
+  });
+}
+
+export function Server({ noServer }) {
+  if (!noServer) { throw new Error('not supported: noServer: false'); }
+
+  const events = makeEmitter();
+  function on(name, handler) {
+    if (!['upgrade', 'connection'].includes(name)) {
+      throw new Error(`not supported: on(${name})`);
+    }
+    events.on(name, handler);
+  }
+
+  on('connection', (ws, req) => {
+    // keep track of ws for closing?
+  });
+
+  return harden({
+    on,
+    handleUpgrade(nodeReq, nodeSocket, head, wsHandler) {
+      // TODO: only handle given path?
+      const { path, host, headers } = nodeReq;
+      const { _xs_socket: socket } = nodeSocket;
+      const protocol = 'TODO';
+      console.log('@@shim TODO!!! actually handle upgrade');
+      console.log('@@ws shim handleUpgrade making WebSocket:', { path, host, headers, protocol, socket });
+      const ws = WebSocket({ path, host, headers, protocol, socket });
+      later(() => wsHandler(ws));
+    },
+    emit: events.emit,
   });
 }
 
