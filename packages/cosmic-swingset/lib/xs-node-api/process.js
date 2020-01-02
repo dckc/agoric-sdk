e0f0ea5f9f493f12489dba0207b02e3be1d07014
Dan Connolly <dckc@madmode.com>
2020-01-02 10:20:54 -0600

    xs-node: integrate chdir with realpathSync

--- packages/cosmic-swingset/lib/xs-node-api/process.js
+++ packages/cosmic-swingset/lib/xs-node-api/process.js
@@ -2,6 +2,7 @@
 import harden from '@agoric/harden';
 
 const theEnv = {}; // ISSUE: global mutable state
+let workDir = '.';
 
 const process = harden({
   on(signal, handler) {
@@ -10,7 +11,14 @@ const process = harden({
   get env() {
     console.warn('environment on xs is TODO');
     return theEnv;
-  }
+  },
+  chdir(path) {
+    console.warn('XS: chdir is a stub');
+    workDir = path;
+  },
+  cwd() {
+    return workDir;
+  },
 });
 
 export default process;
