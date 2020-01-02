7b832b83dd35b9cab6abf33cb342221f45691959
Dan Connolly <dckc@madmode.com>
2020-01-02 11:57:36 -0600

    bin/ag-solo-xs: add dependency on bundles

--- packages/cosmic-swingset/ag-solo-xs.mk
+++ packages/cosmic-swingset/ag-solo-xs.mk
@@ -8,16 +8,14 @@ BASEDIR=t3
 ./bin/ag-solo-xs: $(MODDABLE)/build/bin/lin/release/cosmic-swingset
 	cp $(MODDABLE)/build/bin/lin/release/cosmic-swingset $@
 
-check-start: ./bin/ag-solo-xs ../SwingSet/src/bundles/mailbox-src.js t3/vats/bootstrap-src.js
-	cd $(BASEDIR) && $(MODDABLE)/build/bin/lin/release/cosmic-swingset start --role=three_client
+check-start: ./bin/ag-solo-xs
+	cd $(BASEDIR) && ../bin/ag-solo-xs start --role=three_client
 
-build: $(MODDABLE)/build/bin/lin/release/cosmic-swingset
-
-$(MODDABLE)/build/bin/lin/release/cosmic-swingset: ./bin/ag-solo-xs.js lib/ag-solo/*.js lib/xs-npm/*.js lib/xs-node-global/*.js lib/xs-node-api/*.js lib/ag-solo-todo/*.js
+$(MODDABLE)/build/bin/lin/release/cosmic-swingset: ./bin/ag-solo-xs.js lib/ag-solo/*.js lib/xs-npm/*.js lib/xs-node-global/*.js lib/xs-node-api/*.js lib/ag-solo-todo/*.js ../SwingSet/src/bundles/mailbox-src.js $(BASEDIR)/vats/bootstrap-src.js
 	mcconfig -m -p x-cli-lin ag-solo-xs-manifest.json
 
 # run in simulator and xsbug
-debug-lite: ../SwingSet/src/bundles/mailbox-src.js t3/vats/bootstrap-src.js
+debug-lite: ../SwingSet/src/bundles/mailbox-src.js $(BASDIR)/vats/bootstrap-src.js
 	mcconfig -d -p lin -m ag-solo-xs-manifest.json
 
 force-build:
@@ -26,13 +24,13 @@ force-build:
 
 ### rest of the makefile is not recently tested
 
-t3:
+$(BASEDIR) $(BASEDIR)/vats/bootstrap.js:
 	make scenario3-setup
 
 # bundle all the vats in t3/vats
-t3/vats/bootstrap-src.js: t3/vats/bootstrap.js
-	cd t3/vats && node -r esm ../../../SwingSet/scripts/build-bundle.js bootstrap.js bootstrap-src.js
-	cd t3/vats && for vat in vat-*.js; do \
+$(BASEDIR)/vats/bootstrap-src.js: $(BASEDIR)/vats/bootstrap.js
+	cd $(BASEDIR)/vats && node -r esm ../../../SwingSet/scripts/build-bundle.js bootstrap.js bootstrap-src.js
+	cd $(BASEDIR)/vats && for vat in vat-*.js; do \
 		bundle=`echo $$vat | perl -pe 's/vat-([a-z]+).js/vat_$$1-src.js/'`; \
 		echo $$vat '->' $$bundle; \
 		node -r esm ../../../SwingSet/scripts/build-bundle.js $$vat $$bundle; \
@@ -43,11 +41,12 @@ vat-device-bundles: ../SwingSet/src/bundles/mailbox-src.js
  ../SwingSet/src/bundles/mailbox-src.js:
 	cd ../SwingSet && node -r esm scripts/build-bundle.js
 
-debug-build: vat-device-bundles t3 t3/vats/bootstrap-src.js
+debug-build: vat-device-bundles $(BASEDIR)/vats/bootstrap-src.js
 	mcconfig -d -p lin -m ag-solo-xs-manifest.json
 
 clean:
 	-rm -rf ./bin/ag-solo-xs
+	-rm -rf $(BASEDIR)
 	-rm -rf $(MODDABLE)/build/bin/lin/release/$(PKG)
 	-rm -rf $(MODDABLE)/build/tmp/lin/release/$(PKG)
 	-rm -f $(MODDABLE)/build/bin/lin/release/$(PKG)
