539f0a3a4aac60cd14319b8349c0e77a2bd0826f
Dan Connolly <dckc@madmode.com>
2020-01-18 00:06:24 -0600

    xs require: genesis vats, harden, nat, evaluate
    hmm... overlap with bundle-source

--- packages/cosmic-swingset/lib/xs-node-global/require-xs.js
+++ packages/cosmic-swingset/lib/xs-node-global/require-xs.js
@@ -1,6 +1,20 @@
 /* global globalThis, Compartment */
 const harden = x => Object.freeze(x, true);
 
+import Resource from 'Resource';
+
+// ISSUE: duplicated from bundle-source.js
+const genesisVats = harden({
+  './mailbox-src': '@agoric.mailbox-src.js',
+  './command-src': '@agoric.command-src.js',
+  './timer-src': '@agoric.timer-src.js',
+  '../src/devices/loopbox-src': '@agoric.loopbox-src.js',
+  './vats/comms': '@agoric.comms.js',
+  './vats/vat-timerWrapper': '@agoric.vat-timerWrapper.js',
+  './vats/vat-tp': '@agoric.vattp.js',
+});
+
+
 // ISSUE: duplicated from bundle-source.js
 function butLast(p) {
   const pos = p.lastIndexOf('/');
@@ -16,9 +30,9 @@ function mangleName(path) {
   } else if (path.match(/\/bootstrap.js$/)) {
     bundlePath = `${butLast(path)}bootstrap-src.js`;
   } else {
-    throw new Error(`expected vat-NAME.js; got: ${path}`);
+    throw new Error(`require expected vat-NAME.js; got: ${path}`);
   }
-  console.log(`==@@ require.resolve ${path} -> ${bundlePath}`);
+  console.log(`= xs: require ${path} -> ${bundlePath}`);
   return bundlePath;
 }
 
@@ -30,14 +44,27 @@ function resolve(specifier) {
 
 export function makeRequire(files, globals, modMap, root='ses') {
   function require(specifier) {
-    if (specifier === '@agoric/harden') {
-      return { default: harden };
+    switch (specifier) {
+    case '@agoric/harden':
+      // avoid: Compartment: shared module
+      return harden;
+    case '@agoric/nat':
+    case '@agoric/evaluate':
+      return new Compartment(specifier, {}, modMap).export.default;
+    default:
+      let modText;
+      const resName = genesisVats[specifier];
+      if (resName) {
+	const modROM = new Resource(resName);
+	modText = String.fromArrayBuffer(modROM.slice(0));
+      } else {
+	modText = files.resolve(mangleName(specifier)).readFileSync();
+      }
+      const modExpr = modText.slice('export default '.length);
+      const c = new Compartment(root, { require, ...globals }, modMap);
+      const getExport = c.export.evaluateExpr(modExpr, {});
+      return getExport();
     }
-    const modText = files.resolve(mangleName(specifier)).readFileSync();
-    const modExpr = modText.slice('export default '.length);
-    const c = new Compartment(root, { require, ...globals }, modMap);
-    const getExport = c.export.evaluateExpr(modExpr, {});
-    return getExport();
   }
   require.resolve = resolve;
   return harden(require);
